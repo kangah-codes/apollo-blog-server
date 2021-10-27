b65f002cbec911707b0a5755742adfe66499f3a1
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-27 21:37:38 +0000

    feat: Added post comment resolvers

---
+++ src/graphql/resolvers/PostComment.ts
@@ -0,0 +1,61 @@
+import Blog from "../../models/BlogPost";
+import Comment from "../../models/PostComment";
+
+export const createComment = async (args) => {
+	const { name, comment, blog } = args.input;
+	const postComment = new Comment({
+		name,
+		comment,
+	});
+
+	const result = await postComment.save();
+
+	await Blog.findByIdAndUpdate(blog, {
+		$push: {
+			comments: result.id,
+		},
+	});
+
+	return {
+		...result._doc,
+		id: result.id,
+	};
+};
+
+export const deleteComment = async (args) => {
+	const { id } = args;
+
+	const result = await Comment.findByIdAndDelete(id);
+
+	if (!result) {
+		throw new Error("Comment not found");
+	}
+
+	return {
+		...result._doc,
+		id: result.id,
+	};
+};
+
+export const replyComment = async (args) => {
+	console.log(args);
+	const { name, comment, parentComment } = args.input;
+
+	const replyComment = new Comment({
+		name,
+		comment,
+	});
+
+	const result = await replyComment.save();
+
+	await Comment.findByIdAndUpdate(parentComment, {
+		$push: {
+			replies: result.id,
+		},
+	});
+
+	return {
+		...result._doc,
+		id: result.id,
+	};
+};
