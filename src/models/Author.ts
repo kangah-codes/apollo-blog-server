996a90771aa77dfc3b85fc95b60638d17482ee05
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-27 14:19:19 +0000

    feat: Added author model

---
+++ src/models/Author.ts
@@ -0,0 +1,45 @@
+// create a mongoose model for a blog post author
+
+import { IDocument } from "src/interfaces/db_interfaces";
+import mongoose, {
+	Document,
+	Schema,
+	model,
+	SchemaDefinitionProperty,
+} from "mongoose";
+
+export interface IAuthor extends Document, IDocument<IAuthor> {
+	name: string;
+	email: string;
+	date: SchemaDefinitionProperty<Date>;
+	about: string;
+	avatar: string;
+	createdAt: Date;
+}
+
+export const AuthorSchema = new Schema<IAuthor>({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+	about: {
+		type: String,
+		required: true,
+	},
+	avatar: {
+		type: String,
+		required: false,
+	},
+});
+
+const Author = mongoose.model("Author", AuthorSchema);
+
+export default Author;
