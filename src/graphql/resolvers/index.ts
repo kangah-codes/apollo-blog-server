80fb3e37196146d7eb1e2ef3604e166563e2026d
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-27 21:37:31 +0000

    refactor: Export default

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,35 +1,3 @@
-import { imageUpload } from "../../util/upload";
-import Author from "../../models/Author";
-
-export const authors = async () => {
-	const authors = await Author.find();
-
-	return authors.map((author) => {
-		return {
-			...author._doc,
-			id: author.id,
-		};
-	});
-};
-
-export const createAuthor = async (args) => {
-	const { name, email, date, about, avatar } = args.input;
-	const location = avatar
-		? await imageUpload(avatar, `authors/${email}`)
-		: "https://apollo-blog-server.sfo3.digitaloceanspaces.com/authors/avatar.png";
-
-	const author = new Author({
-		name,
-		email,
-		date,
-		about,
-		avatar: location,
-	});
-
-	const result = await author.save();
-
-	return {
-		...result._doc,
-		id: result.id,
-	};
-};
+export * from "./Author";
+export * from "./BlogPost";
+export * from "./PostComment";
