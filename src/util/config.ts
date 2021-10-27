43ceec147d879028ea9fce2bb1ca90f985017700
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-27 19:13:14 +0000

    feat: Configured env

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,7 +1,10 @@
-export const Env = () => {
+export const Env = (env: NodeJS.ProcessEnv) => {
 	return {
 		production: {
-			databaseURI: process.env.DATABASE_URI,
+			databaseURI: env.DATABASE_URI,
+			AWS_ACCESS_KEY_ID: env.AWS_ACCESS_KEY_ID,
+			AWS_SECRET_ACCESS_KEY: env.AWS_SECRET_ACCESS_KEY,
+			AWS_BUCKET: env.AWS_BUCKET,
 		},
 	};
 };
