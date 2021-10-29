fbdc8a872c067f491829ebe1b8ffa9fc6e0c41a8
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-29 11:47:33 +0000

    refactor: Added return type for Env function

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,7 +1,9 @@
-export const Env = (env: NodeJS.ProcessEnv) => {
+import { IEnv } from "../interfaces/app_interfaces";
+
+export const Env = (env: NodeJS.ProcessEnv): IEnv => {
 	return {
 		production: {
-			databaseURI: env.DATABASE_URI,
+			DATABASE_URI: env.DATABASE_URI,
 			AWS_ACCESS_KEY_ID: env.AWS_ACCESS_KEY_ID,
 			AWS_SECRET_ACCESS_KEY: env.AWS_SECRET_ACCESS_KEY,
 			AWS_BUCKET: env.AWS_BUCKET,
