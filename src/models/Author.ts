3e0a6b2003991439eef858c3cbc63eccff19b6f8
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-29 11:26:30 +0000

    refactor: Changed import for interfaces

--- src/models/Author.ts
+++ src/models/Author.ts
@@ -1,21 +1,12 @@
 // create a mongoose model for a blog post author
 
-import { IDocument } from "src/interfaces/db_interfaces";
+import { IAuthor, IDocument } from "src/interfaces/db_interfaces";
 import mongoose, {
 	Document,
 	Schema,
 	model,
 	SchemaDefinitionProperty,
 } from "mongoose";
-
-export interface IAuthor extends Document, IDocument<IAuthor> {
-	name: string;
-	email: string;
-	date: SchemaDefinitionProperty<Date>;
-	about: string;
-	avatar: string;
-	createdAt: Date;
-}
 
 export const AuthorSchema = new Schema<IAuthor>({
 	name: {
