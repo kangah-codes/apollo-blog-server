1f2372683a25f0a62070cf0dc7a75cac22293ccd
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-29 11:26:18 +0000

    refactor: Moved interfaces into one file

--- src/interfaces/db_interfaces.ts
+++ src/interfaces/db_interfaces.ts
@@ -1,3 +1,33 @@
+import { PopulatedDoc, SchemaDefinitionProperty } from "mongoose";
+
 export interface IDocument<T> {
 	_doc: T;
+}
+
+export interface IAuthor extends Document, IDocument<IAuthor> {
+	name: string;
+	email: string;
+	date: SchemaDefinitionProperty<Date>;
+	about: string;
+	avatar: string;
+	createdAt: Date;
+}
+
+export interface IBlog extends Document, IDocument<IBlog> {
+	title: string;
+	content: string;
+	date: SchemaDefinitionProperty<Date>;
+	author: PopulatedDoc<IAuthor>;
+	comments: PopulatedDoc<IComment>;
+	likes: number;
+	dislikes: number;
+	banner: string;
+	slug: string;
+}
+
+export interface IComment extends Document, IDocument<IComment> {
+	name: string;
+	comment: string;
+	replies: Array<PopulatedDoc<IComment>>;
+	post: PopulatedDoc<IBlog>;
 }
